a8f11e03ef33b398b56ac8ff8418c470412015fa
trk2562 <yilmaz.tarik@ogr.deu.edu.tr>
2022-08-02 09:23:01 +0300

    11.Ders 1.Ödev

---
+++ WebAPI/Controllers/RentalOrdersController.cs
@@ -0,0 +1,73 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentalOrdersController : ControllerBase
+    {
+        IRentalOrderService _rentalOrderService;
+
+        public RentalOrdersController(IRentalOrderService rentalOrderService)
+        {
+            _rentalOrderService = rentalOrderService;
+        }
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            var result = _rentalOrderService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpGet("GetCarDetails")]
+        public IActionResult GetCarDetails()
+        {
+            var result = _rentalOrderService.GetCarDetails();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPost("Add")]
+        public IActionResult Add(RentalOrder rentalOrder)
+        {
+            var result = _rentalOrderService.Add(rentalOrder);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPost("Delete")]
+        public IActionResult Delete(RentalOrder rentalOrder)
+        {
+            var result = _rentalOrderService.Delete(rentalOrder);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPost("Update")]
+        public IActionResult Update(RentalOrder rentalOrder)
+        {
+            var result = _rentalOrderService.Update(rentalOrder);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
