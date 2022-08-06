7c59cd65eea361649b8bb4a7dbe4f227a0c7540c
Tarık Yılmaz <yilmaz.tarik@ogr.deu.edu.tr>
2022-08-06 11:23:05 +0300

    12.Hafta

---
+++ Business/ValidationRules/CarValidator.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(p => p.CarId).NotEmpty();
+            RuleFor(p => p.Model).MinimumLength(2);
+            RuleFor(P => P.BrandId).NotEmpty();
+            //RuleFor(p => p.).GreaterThan(0);
+            //RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1);
+            //RuleFor(p => p.ProductName).Must(StartWithA).WithMessage("Ürünler A harfi ile başlamalı");
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
