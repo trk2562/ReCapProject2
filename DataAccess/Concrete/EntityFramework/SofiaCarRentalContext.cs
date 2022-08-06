a2ccdc9be4102e01ffe69fff71b70f687e3cdd28
Tarık Yılmaz <yilmaz.tarik@ogr.deu.edu.tr>
2022-08-06 11:23:05 +0300

    12.Hafta

--- DataAccess/Concrete/EntityFramework/SofiaCarRentalContext.cs
+++ DataAccess/Concrete/EntityFramework/SofiaCarRentalContext.cs
@@ -10,7 +10,7 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=SofiaCarRental;Trusted_Connection=true");
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=SofiaCarRental1;Trusted_Connection=true");
         }
         public DbSet<Car> Cars { get; set; }
         public DbSet<Brand> Brands { get; set; }
